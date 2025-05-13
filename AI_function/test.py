3143fcae117cddd2b45300888a1562635bed842c
Blocky060 <166016583+Blocky060@users.noreply.github.com>
2025-05-13 17:56:50 +0900

    real end

--- AI_function/test.py
+++ AI_function/test.py
@@ -1,9 +1,11 @@
 import ai1_function
 import ai2_function
 
+
 #a = (ai1_function.AI_grop('Hello'))
 #print (a)
 #a = "1+4 = 8"
 #print (ai2_function.AI_gemini("Hello"))
+
 
 #print(ai2_function.AI_gemini("지금 한국기준 날씨가 어떄?"))
